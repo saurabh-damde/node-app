a0998bf445c4fc93aac25fce9e97e7e905d2743b
Saurabh Damde <saurabhdamde@gmail.com>
2024-07-12 14:47:44 +0530

    Implement Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,20 @@
 const path = require("path");
 
 const express = require("express");
-
-const rootDir = require("../utils/path");
 const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    pageTitle: "Shop",
+    path: "/",
+    products: products,
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true,
+  });
 });
 
 module.exports = router;
